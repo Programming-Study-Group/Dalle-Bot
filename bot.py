6025bda72be2c987c777702e9328f192edcc9a71
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2022-06-18 14:01:50 +0300

    added part to parse response of the request and generate base64 image (bug)

--- bot.py
+++ bot.py
@@ -3,6 +3,14 @@ from settings import settings
 
 # discord
 import discord
+from requests import post
+
+# regex
+import re
+
+# base64
+import io
+from base64 import b64decode, b64encode
 
 client = discord.Client()
 
@@ -17,5 +25,24 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('$generate:'):
+        await message.channel.send("Making the request - it may take a while")
+        req = post(url = settings.URL, json = {'prompt': "yoda"})
+        await message.channel.send("Result is completed")
+        
+        images = re.search('\[(.*)\]', req.content.decode("utf-8") ).group(0)
+        encodings = [e.replace("\\n", "%0A") for e in re.findall('"([^"]*)"', images)]
+        for encoded in encodings:
+            # for the padding
+            a = b64decode(encoded + "===")
+            print(encoded, "\n\n\n")
+            print(b64encode(a), "\n\n\n")
+            print(b64encode(a) == encoded) # supposed to return true - but false
+            
+            with open("imageToSave.png", "wb") as fh:
+                fh.write(b64decode(encoded))
+                file = discord.File(io.BytesIO(b64decode(encoded)))
+                await message.channel.send(file = file)
 
 client.run(settings.TOKEN)
--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 # token (stored in .env file)
-
 from dotenv import load_dotenv
 from os import getenv
 
 class Settings:
     load_dotenv()
     TOKEN = getenv('TOKEN')
+    URL = "https://bf.dallemini.ai/generate"
 
 settings = Settings()
